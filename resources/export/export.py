a5ee262858751e1aeb38090da31de8007f9b9179
Andrea Bonomi <andrea.bonomi@gmail.com>
2024-01-09 22:43:33 +0100

    resources

---
+++ resources/export/export.py
@@ -0,0 +1,159 @@
+import io
+import json
+import sys
+from dataclasses import dataclass
+from enum import Enum
+from pathlib import Path
+from typing import BinaryIO
+
+import cstruct
+
+from windef import (
+    STRINGS_BLOCK_SIZE,
+    BitmapFileHeader,
+    BitmapInfoHeader,
+    BitmapV4Header,
+    BitmapV5Header,
+    CieXYZ,
+    CieXYZTriple,
+    CursorDir,
+    IconDirectoryEntry,
+    IconResDir,
+    NewHeader,
+    ResDir,
+    ResourceHeaderPost,
+    ResourceHeaderPre,
+    ResourceType,
+)
+
+
+@dataclass
+class ResourceItem:
+    name: str
+    type: ResourceType
+    data: bytes
+
+
+base_path = Path.cwd() / ".."
+bitmaps_dir = base_path / "bitmaps"
+icons_dir = base_path / "icons"
+strings_dir = base_path / "strings"
+
+
+def read_word(f: BinaryIO) -> int:
+    return ord(f.read(1)) + ord(f.read(1)) * 256
+
+
+def read_str_or_id(f: BinaryIO) -> str | int:
+    chars = [f.read(1), f.read(1)]
+    if chars == [b"\xff", b"\xff"]:
+        return read_word(f)
+    while True:
+        c0 = f.read(1)
+        c1 = f.read(1)
+        if c0 == b"\x00":
+            # print(chars)
+            return (b"".join(chars)).decode("UTF-16LE")
+        chars.append(c0)
+        chars.append(c1)
+
+
+def read_res(filename: str):
+    res_file = Path(filename)
+    with res_file.open("rb") as f:
+        while True:
+            pos = f.tell()
+            pre_data = f.read(len(ResourceHeaderPre))
+            if not pre_data:
+                break
+            pre = ResourceHeaderPre(pre_data)
+            res_type = read_str_or_id(f)
+            res_name = read_str_or_id(f)
+            # print(f"{pre} {res_type=} {res_name=}")
+            post = ResourceHeaderPost(f.read(len(ResourceHeaderPost)))
+            pos2 = f.tell()
+            f.read(pre.HeaderSize - pos2 + pos)
+            data = f.read(pre.DataSize)
+            if pre.DataSize % 4:
+                padding = 4 - (pre.DataSize % 4)
+                f.read(padding)
+            yield ResourceItem(name=res_name, type=ResourceType(res_type), data=data)
+
+
+def export_icon(
+    resource: ResourceItem,
+    resources: list[ResourceItem],
+    icons: dict[str, ResourceItem],
+) -> None:
+    icons_dir.mkdir(exist_ok=True)
+    header = NewHeader(resource.data)
+    res_dir = ResDir(resource.data[len(header) :])
+    # print(res_dir.Icon, res_dir.IconCursorId)
+    icon = icons.get(res_dir.IconCursorId)
+    icon_dir_entry = IconDirectoryEntry()
+    icon_dir_entry.bWidth = res_dir.Icon.Width
+    icon_dir_entry.bHeight = res_dir.Icon.Width
+    icon_dir_entry.wBitCount = res_dir.BitCount
+    icon_dir_entry.wPlanes = res_dir.Planes
+    bColorCount = 1 << (icon_dir_entry.wBitCount * icon_dir_entry.wPlanes)
+    icon_dir_entry.bColorCount = bColorCount if bColorCount < 8 else 0
+    icon_dir_entry.dwBytesInRes = res_dir.BytesInRes
+    icon_dir_entry.dwImageOffset = len(header) + len(icon_dir_entry)
+    icon_file = icons_dir / f"{resource.name}.ico"
+    icon_file.write_bytes(header.pack() + icon_dir_entry.pack() + icon.data)
+
+
+def export_bitmap(resource: ResourceItem, resources: list[ResourceItem]) -> None:
+    bitmaps_dir.mkdir(exist_ok=True)
+    bitmap_header = BitmapInfoHeader(resource.data)
+    header = BitmapFileHeader(
+        bfType=0x4D42,  # BM
+        bfSize=len(resource.data),
+        bfReserved1=0,
+        bfReserved2=0,
+        bfOffBits=len(BitmapFileHeader)
+        + len(resource.data)
+        - bitmap_header.biSizeImage,
+        # bfOffBits=bitmap_header.biSize + len(BitmapFileHeader),
+    )
+    # print(header, len(header))
+    bitmap_file = bitmaps_dir / f"{resource.name}.bmp"
+    bitmap_file.write_bytes(header.pack() + resource.data)
+
+
+def export_strings(resource: ResourceItem, resources: list[ResourceItem]) -> None:
+    strings_dir.mkdir(exist_ok=True)
+    base = (resource.name - 1) << 4
+    data = {}
+    with io.BytesIO(resource.data) as f:
+        for i in range(0, STRINGS_BLOCK_SIZE):
+            string_len = read_word(f)
+            if string_len:
+                string_id = base + i
+                t = f.read(string_len * 2).decode("UTF-16LE")
+                # print(f"{string_id=} {t=}")
+                data[str(string_id)] = t
+    print(json.dumps(data))
+    strings_file = strings_dir / f"{resource.name}.json"
+    with strings_file.open("w", encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
+        f.write("\n")
+
+
+def main() -> None:
+    for filename in sys.argv[1:]:
+        resources = list(read_res(filename))
+        icons = {x.name: x for x in resources if x.type == ResourceType.icon}
+
+        for resource in resources:
+            print(f"{resource.name} {resource.type}")
+            if resource.type == ResourceType.group_icon:
+                export_icon(resource, resources, icons)
+            elif resource.type == ResourceType.bitmap:
+                export_bitmap(resource, resources)
+            elif resource.type == ResourceType.string:
+                export_strings(resource, resources)
+
+
+if __name__ == "__main__":
+    main()
---
+++ resources/export/windef.py
@@ -0,0 +1,269 @@
+from enum import Enum
+
+import cstruct
+
+__all__ = [
+    "STRINGS_BLOCK_SIZE",
+    "ResourceType",
+    "ResourceHeaderPre",
+    "ResourceHeaderPost",
+    "NewHeader",
+    "IconResDir",
+    "CursorDir",
+    "ResDir",
+    "IconDirectoryEntry",
+    "BitmapFileHeader",
+    "CieXYZ",
+    "CieXYZTriple",
+    "BitmapInfoHeader",
+    "BitmapV4Header",
+    "BitmapV5Header",
+]
+
+STRINGS_BLOCK_SIZE = 16
+
+
+class ResourceType(Enum):
+    none = 0
+    cursor = 1
+    bitmap = 2
+    icon = 3
+    menu = 4
+    dialog = 5
+    string = 6
+    fontdir = 7
+    font = 8
+    accelerator = 9
+    rcdata = 10
+    messagetable = 11
+    group_cursor = 12
+    group_icon = 14
+    version = 16
+    dlginclude = 17
+    plugplay = 19
+    vxd = 20
+    anicursor = 21
+    aniicon = 22
+    html = 23
+    manifest = 24
+
+
+cstruct.parse(
+    """
+    typedef unsigned char BYTE;
+    typedef unsigned short WORD;
+    typedef unsigned long DWORD;
+    typedef long LONG;
+    typedef long FXPT2DOT30;
+"""
+)
+
+
+class ResourceHeaderPre(cstruct.MemCStruct):
+    __byte_order__ = cstruct.LITTLE_ENDIAN
+    __def__ = """
+        struct {
+            DWORD DataSize;
+            DWORD HeaderSize;
+        }
+    """
+
+
+class ResourceHeaderPost(cstruct.MemCStruct):
+    __byte_order__ = cstruct.LITTLE_ENDIAN
+    __def__ = """
+        struct {
+            DWORD DataVersion;
+            WORD  MemoryFlags;
+            WORD  LanguageId;
+            DWORD Version;
+            DWORD Characteristics;
+        }
+    """
+
+
+class NewHeader(cstruct.MemCStruct):
+    __byte_order__ = cstruct.LITTLE_ENDIAN
+    __def__ = """
+        struct {
+            WORD Reserved;
+            WORD ResType;
+            WORD ResCount;
+        }
+    """
+
+
+class IconResDir(cstruct.MemCStruct):
+    __byte_order__ = cstruct.LITTLE_ENDIAN
+    __def__ = """
+        struct {
+            BYTE Width;
+            BYTE Height;
+            BYTE ColorCount;
+            BYTE reserved;
+        }
+    """
+
+
+class CursorDir(cstruct.MemCStruct):
+    __byte_order__ = cstruct.LITTLE_ENDIAN
+    __def__ = """
+        struct {
+            BYTE Width;
+            BYTE Height;
+        }
+    """
+
+
+class ResDir(cstruct.MemCStruct):
+    __byte_order__ = cstruct.LITTLE_ENDIAN
+    __def__ = """
+        typedef struct IconResDir ICONRESDIR;
+        typedef struct CursorDir CURSORDIR;
+
+        struct {
+            union
+            {
+                ICONRESDIR   Icon;
+                CURSORDIR    Cursor;
+            };
+            WORD       Planes;
+            WORD       BitCount;
+            DWORD      BytesInRes;
+            WORD       IconCursorId;
+        }
+    """
+
+
+class IconDirectoryEntry(cstruct.MemCStruct):
+    __byte_order__ = cstruct.LITTLE_ENDIAN
+    __def__ = """
+        struct {
+            BYTE  bWidth;
+            BYTE  bHeight;
+            BYTE  bColorCount;
+            BYTE  bReserved;
+            WORD  wPlanes;
+            WORD  wBitCount;
+            DWORD dwBytesInRes;
+            DWORD dwImageOffset;
+        }
+    """
+
+
+class BitmapFileHeader(cstruct.MemCStruct):
+    __byte_order__ = cstruct.LITTLE_ENDIAN
+    __def__ = """
+        struct {
+            WORD  bfType;
+            DWORD bfSize;
+            WORD  bfReserved1;
+            WORD  bfReserved2;
+            DWORD bfOffBits;
+        }
+    """
+
+
+class CieXYZ(cstruct.MemCStruct):
+    __byte_order__ = cstruct.LITTLE_ENDIAN
+    __def__ = """
+        struct {
+            FXPT2DOT30 ciexyzX;
+            FXPT2DOT30 ciexyzY;
+            FXPT2DOT30 ciexyzZ;
+        }
+    """
+
+
+class CieXYZTriple(cstruct.MemCStruct):
+    __byte_order__ = cstruct.LITTLE_ENDIAN
+    __def__ = """
+        typedef struct CieXYZ CIEXYZ;
+
+        struct {
+            CIEXYZ ciexyzRed;
+            CIEXYZ ciexyzGreen;
+            CIEXYZ ciexyzBlue;
+        }
+    """
+
+
+class BitmapInfoHeader(cstruct.MemCStruct):
+    __byte_order__ = cstruct.LITTLE_ENDIAN
+    __def__ = """
+        typedef struct CieXYZTriple CIEXYZTRIPLE;
+
+        struct {
+            DWORD biSize;
+            LONG  biWidth;
+            LONG  biHeight;
+            WORD  biPlanes;
+            WORD  biBitCount;
+            DWORD biCompression;
+            DWORD biSizeImage;
+            LONG  biXPelsPerMeter;
+            LONG  biYPelsPerMeter;
+            DWORD biClrUsed;
+            DWORD biClrImportant;
+        }
+    """
+
+
+class BitmapV4Header(cstruct.MemCStruct):
+    __byte_order__ = cstruct.LITTLE_ENDIAN
+    __def__ = """
+        struct {
+          DWORD        bV4Size;
+          LONG         bV4Width;
+          LONG         bV4Height;
+          WORD         bV4Planes;
+          WORD         bV4BitCount;
+          DWORD        bV4V4Compression;
+          DWORD        bV4SizeImage;
+          LONG         bV4XPelsPerMeter;
+          LONG         bV4YPelsPerMeter;
+          DWORD        bV4ClrUsed;
+          DWORD        bV4ClrImportant;
+          DWORD        bV4RedMask;
+          DWORD        bV4GreenMask;
+          DWORD        bV4BlueMask;
+          DWORD        bV4AlphaMask;
+          DWORD        bV4CSType;
+          CIEXYZTRIPLE bV4Endpoints;
+          DWORD        bV4GammaRed;
+          DWORD        bV4GammaGreen;
+          DWORD        bV4GammaBlue;
+        }
+    """
+
+
+class BitmapV5Header(cstruct.MemCStruct):
+    __byte_order__ = cstruct.LITTLE_ENDIAN
+    __def__ = """
+        struct {
+          DWORD        bV5Size;
+          LONG         bV5Width;
+          LONG         bV5Height;
+          WORD         bV5Planes;
+          WORD         bV5BitCount;
+          DWORD        bV5Compression;
+          DWORD        bV5SizeImage;
+          LONG         bV5XPelsPerMeter;
+          LONG         bV5YPelsPerMeter;
+          DWORD        bV5ClrUsed;
+          DWORD        bV5ClrImportant;
+          DWORD        bV5RedMask;
+          DWORD        bV5GreenMask;
+          DWORD        bV5BlueMask;
+          DWORD        bV5AlphaMask;
+          DWORD        bV5CSType;
+          CIEXYZTRIPLE bV5Endpoints;
+          DWORD        bV5GammaRed;
+          DWORD        bV5GammaGreen;
+          DWORD        bV5GammaBlue;
+          DWORD        bV5Intent;
+          DWORD        bV5ProfileData;
+          DWORD        bV5ProfileSize;
+          DWORD        bV5Reserved;
+        }
+    """
